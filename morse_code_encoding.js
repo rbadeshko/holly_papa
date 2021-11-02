565224bbc3b77aee0b185851a5389f862394deaa
Roman BadeschkoRomeo <romeo@Romans-MacBook-Pro.local>
2021-11-02 13:37:54 +0100

    test remote commit VSCode

---
+++ morse_code_encoding.js
@@ -0,0 +1,8 @@
+decodeMorse = (morseCode) => {
+    return letter2 = morseCode.trim().split('  ').map( item => { //whole sentense, three spaces in MorceCode === one normal space between words
+      return item.split(" ").map(item2 => { //Every Words
+        return MORSE_CODE[item2] //Every Letter
+      }).join('')
+    }).join(" ");
+  console.log(letter2);   
+  }
