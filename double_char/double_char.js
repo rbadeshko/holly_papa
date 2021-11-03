55905377481d0d03a27614b15a6e9595f1948f50
Roman Badeschko <rbadeshko@gmail.com>
2021-11-03 11:35:33 +0100

    refactored

--- double_char/double_char.js
+++ double_char/double_char.js
@@ -1,5 +1,5 @@
 function doubleChar(str) {
-    return str.split("").map((item) => {return item += item;}).join('');
+    return str.split("").map( item => { item + item }).join('');
   }
-  let str = "String_! .";
+  let str = "String_! .ASD";
   console.log(doubleChar(str));
