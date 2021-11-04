a32a5a67ba89dc5421b255868169ad5a110babc3
Roman Badeschko <rbadeshko@gmail.com>
2021-11-04 20:58:48 +0100

    refactored and tested

--- tel_number_format/tel_number_format.js
+++ tel_number_format/tel_number_format.js
@@ -1,2 +1,4 @@
-
-createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+const createPhoneNumber = (numbers) => {
+    let s = numbers.join("");
+    return "(" + s.slice(0, 3) + ") " + s.slice(3, 6) + "-" + s.slice(-4);
+  };
