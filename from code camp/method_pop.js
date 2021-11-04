be8865553319130235768b45cb43d3f161c4d971
Roman Badeschko <rbadeshko@gmail.com>
2021-11-04 16:17:21 +0100

    method pop()

---
+++ from code camp/method_pop.js
@@ -0,0 +1,6 @@
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+const removedFromMyArray = myArray.pop();
+// Only change code below this line
+console.log(myArray); // Arr without last element
+console.log(removedFromMyArray); //removed elmnt
