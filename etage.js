7709f35e56842f0bbe77d3b9373bc865e4d2ac34
Roman Badeschko <rbadeshko@gmail.com>
2021-11-07 11:09:50 +0100

    refactored with Math.abs

--- etage.js
+++ etage.js
@@ -1,7 +1,8 @@
 const elevatorDistance = (array) => {
     let etage = 0;
     for (let i = 0; i < array.length - 1; i++) {
-      etage += Math.pow(Math.pow(array[i] - array[i + 1], 2), 0.5);
+      //etage += Math.pow(Math.pow(array[i] - array[i + 1], 2), 0.5);
+      etage += Math.abs(array[i] - array[i + 1]) // new code
     }
     return etage;
     
