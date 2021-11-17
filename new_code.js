90a85b43bc879151e496f7b4f9863d7313b6ba6b
Roman Badeschko <rbadeshko@gmail.com>
2021-11-17 12:44:05 +0100

    new code

---
+++ new_code.js
@@ -0,0 +1,20 @@
+// Declare the myGlobal variable below this line
+
+
+function fun1() {
+    // Assign 5 to oopsGlobal Here
+  
+  }
+  
+  // Only change code above this line
+  
+  function fun2() {
+    var output = "";
+    if (typeof myGlobal != "undefined") {
+      output += "myGlobal: " + myGlobal;
+    }
+    if (typeof oopsGlobal != "undefined") {
+      output += " oopsGlobal: " + oopsGlobal;
+    }
+    console.log(output);
+  }
