c839b4b6a06ec64175a4dd83eadf1341ebd1c0fe
Roman Badeschko <rbadeshko@gmail.com>
2021-11-18 17:54:39 +0100

    switch

---
+++ switch.js
@@ -0,0 +1,24 @@
+function caseInSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+    switch(val) {
+      case 1: 
+        answer = "alpha";
+        //console.log("val");
+        break;
+      case 2: 
+        answer = "beta";
+        break;
+      case 3: 
+        answer = "gamma";
+        break;
+      case 4: 
+        answer = "delta";
+        break;
+    }
+    console.log(answer)
+  
+  
+    // Only change code above this line
+    return answer;
+  }
