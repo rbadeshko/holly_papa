b880dfe7ede08623e8c909dfe186ebe2faee9f28
Roman Badeschko <rbadeshko@gmail.com>
2021-11-18 18:35:24 +0100

    bouncingBall solved

---
+++ bouncingBall/bouncingBall.js
@@ -0,0 +1,17 @@
+//l6
+function bouncingBall(h,  bounce,  window) {
+    let num = 1;
+    if (h > 0 && bounce > 0 && bounce < 1 && h > window) {
+      for (let i = 0; i < 10; i++) {
+        if (h * bounce > window) {
+          h *= bounce;
+          num += 2;
+        }
+      }
+  
+      console.log(num);
+      return num;
+    }
+    return -1;
+    //console.log();
+  }
