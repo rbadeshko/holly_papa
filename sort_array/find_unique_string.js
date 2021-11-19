8f65380753cdee0ffd20d24394cdf1ad4a0f776c
Roman Badeschko <rbadeshko@gmail.com>
2021-11-19 15:37:17 +0100

    commen code

--- sort_array/find_unique_string.js
+++ sort_array/find_unique_string.js
@@ -1,7 +1,7 @@
 //l6
 const findUniq2 = (arr) => {
     let newArr = arr.map((a) => [...new Set(a.toLowerCase())].sort().join(""));
-    console.log(newArr);
+    //console.log(newArr);
     //console.log(newArr.indexOf(newArr[3]));
     for ( let i = 0; i < newArr.length; i++ ) { 
       if ( newArr.indexOf(newArr[i]) === newArr.lastIndexOf(newArr[i]) ) return arr[i] }
