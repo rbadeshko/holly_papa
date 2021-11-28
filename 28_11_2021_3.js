9a6379a950dd366921aee40d73238f3c362d40f7
Roman Badeschko <rbadeshko@gmail.com>
2021-11-28 18:12:05 +0100

    new 2

--- 28_11_2021_3.js
+++ 28_11_2021_3.js
@@ -5,6 +5,6 @@ const testObj = {
     "the drink": "water"
   };
   
-  // Only change code below this line
+  //Only change code below this line 234
   const entreeValue = testObj["an entree"];   // Change this line
   const drinkValue = testObj["the drink"];    // Change this line
