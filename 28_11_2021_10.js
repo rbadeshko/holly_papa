6ef156d869be7d08d2bf632b44f8845346201ca4
Roman Badeschko <rbadeshko@gmail.com>
2021-11-28 18:45:52 +0100

    10

---
+++ 28_11_2021_10.js
@@ -0,0 +1,24 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "Billy Jsdfoel",
+      "title": "Piano sdfMan",
+      "release_year": 1974,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ]
+  
+    }
+  ];
