87be8c11ead41eab9cccc301ef378cdda35888fc
Roman Badeschko <rbadeshko@gmail.com>
2021-11-28 18:09:14 +0100

    28 nov 1

---
+++ 28_11_2021.js
@@ -0,0 +1,10 @@
+const myDog = {
+    // Only change code below this line
+    "name": "dkjsdf",
+    "legs": 1,
+    "tails": 2,
+    "friends": ["Water", "Some"]
+  
+  
+    // Only change code above this line
+  };
