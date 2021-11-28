9be969005915f37afe0a0509d85b9f7c30f0dbf2
Roman Badeschko <rbadeshko@gmail.com>
2021-11-28 18:41:53 +0100

    9

---
+++ 28_9.js
@@ -0,0 +1,12 @@
+function checkObj(obj, checkProp) {
+    // Only change code below this line
+    if(!obj.hasOwnProperty(checkProp)){
+      //console.log(obj.hasOwnProperty(checkProp))
+       return "Not Found";
+    }
+    //console.log(obj) 
+    //console.log(checkProp)
+    console.log(obj[checkProp])
+    return obj.[checkProp];
+    // Only change code above this line
+  }
